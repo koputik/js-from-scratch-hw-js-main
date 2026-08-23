a13ef9035fd6bcb2d52e52159e4ba3fe45844ebe
Maxim Putyutin <maksimpututin@gmail.com>
2026-08-23 22:30:56 +0300

    task 02-2

--- lesson-02/02.js
+++ lesson-02/02.js
@@ -7,9 +7,9 @@
 * Для успешного прохождения тестов не меняйте названия переменных!
 */
 
-let dice1 // your code
-let dice2 // your code
-let isWinningDouble // your code
+let dice1 = Math.round(Math.random()*6 + 1)
+let dice2 = Math.round(Math.random()*6 + 1)
+let isWinningDouble = dice1 > 3 === dice2 > 3 ? true : false
 
 /*
 console.log('Первый бросок: ' + dice1)
