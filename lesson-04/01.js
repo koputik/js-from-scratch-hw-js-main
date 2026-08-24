16fb066049bf168127cfd957ad61dcd47b52dfac
Maxim Putyutin <maksimpututin@gmail.com>
2026-08-24 19:45:39 +0300

    task 04-1

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -14,12 +14,11 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
 function includesElement(array, element) {
-    for (let i = 0; i <= array.length; i++) {
-        if ( array[i] === element ) {
-            return true
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return true;
         }
     }
-    return false
-}
 
-includesElement()
+    return false;
+}
